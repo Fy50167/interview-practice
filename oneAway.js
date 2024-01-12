d6345a0e1b643d74943441d861ae615e105bc34b
Francis Yang <fy50167@gmail.com>
2024-01-11 16:47:49 -0800

    insert and remove one function working with test cases

--- oneAway.js
+++ oneAway.js
@@ -3,3 +3,33 @@
 remove a character, or replace a character. Given two strings, write a function to check if they are
 one edit (or zero edits) away. 
  */
+
+const insertRemove = function(str1, str2) {
+    let pointer = 0;
+    let marker = false;
+    let shortString = '';
+    let longString = '';
+    if (str1.length < str2.length) { // This if statement is super unoptimized; there should be some way to instantly declare the shorter and longer strings (I think?).
+        shortString = str1;
+        longString = str2;
+    } else if (str2.length < str1.length) {
+        shortString = str2;
+        longString = str1;
+    }
+    for (let i = 0; i < shortString.length; i++) {
+        if (shortString[i] !== longString[pointer]) {
+            if (marker === false) {
+                i = i - 1;
+                marker = true;
+            } else {
+                return false
+            }
+        }
+        pointer++;
+    }
+    return true
+}
+
+console.log(insertRemove('pale', 'ple'));
+console.log(insertRemove('pales', 'pale'));
+console.log(insertRemove('pales', 'pald'));
