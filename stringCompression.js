8a0e2db1d9154bddf69aba1b0155c06fed353f6d
Francis Yang <fy50167@gmail.com>
2024-01-11 17:13:01 -0800

    completing string compression function with test cases

--- stringCompression.js
+++ stringCompression.js
@@ -4,3 +4,20 @@ of repeated characters. For example, the string aabcccccaaa would become a2blc5a
 "compressed" string would not become smaller than the original string, your method should return
 the original string. You can assume the string has only uppercase and lowercase letters (a - z). 
  */
+
+const stringCompression = function(str) {
+    let output = '';
+    for (let i = 0; i < str.length; i++) {
+        let count = 1;
+        while (str[i] === str[i + 1]) {
+            i++;
+            count++;
+        }
+        output = output.concat(str[i] + parseInt(count));
+    }
+    
+    return output
+}
+
+console.log(stringCompression('aabcccccaaa'));
+console.log(stringCompression('AabccCCcaaa'));
