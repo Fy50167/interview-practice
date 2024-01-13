c3bc1e81a574eb3eb046d73b169355dda826b552
Francis Yang <fy50167@gmail.com>
2024-01-13 02:15:51 -0800

    finish remove dupes function

--- linked-lists/removeDupes.js
+++ linked-lists/removeDupes.js
@@ -1,3 +1,15 @@
 /**
  * Write code to remove duplicates from an unsorted linked list. 
  */
+
+const removeDupes = function(list) {
+    const hashTable = new Set();
+    let node = list.head;
+    while (node.next !== null) {
+        if (hashTable.has(node.next.value)) {
+            node.next = node.next.next;
+        }
+        node = node.next;
+    }
+    return list
+}
