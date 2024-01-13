9b03b90a77501f1b7e9aa7427c0987c311bbf002
Francis Yang <fy50167@gmail.com>
2024-01-12 21:35:55 -0800

    finishing string rotation

--- stringRotation.js
+++ stringRotation.js
@@ -3,3 +3,14 @@
 of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
 call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat"). 
  */
+
+const stringRotation = function(str1, str2) {
+    if (str2.isSubstring(str1 + str1)) {
+        return true
+    }
+    return false
+}
+
+// Look for logic! This question is so easy if you just fully understand what a rotation really is,
+
+console.log(stringRotation())
