443df24d5badf012a7969482296de29aff586a9d
Francis Yang <fy50167@gmail.com>
2024-02-12 23:15:22 -0800

    finish partition function

--- linked-lists/partition.js
+++ linked-lists/partition.js
@@ -2,3 +2,23 @@
 before all nodes greater than or equal to x. If x is contained within the list, the values of x only need
 to be after the elements less than x (see below). The partition element x can appear anywhere in the
 "right partition"; it does not need to appear between the left and right partitions.  */
+
+var partition = function(head, x) {
+    let list1 = new ListNode(null);
+    let current1 = list1;
+    let list2 = new ListNode(null);
+    let current2  = list2;
+    while (head) {
+        if (head.val < x) {
+            current1.next = head;
+            current1 = current1.next;
+        } else {
+            current2.next = head;
+            current2 = current2.next;
+        }
+        head = head.next;
+    }
+    current2.next = null;
+    current1.next = list2.next;
+    return list1.next
+};
