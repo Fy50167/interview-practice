2231ab7705e00f1d29ae511177bce4c4e1f98005
Francis Yang <fy50167@gmail.com>
2024-02-14 02:00:45 -0800

    index of loop solution

--- linked-lists/loopDetection.js
+++ linked-lists/loopDetection.js
@@ -5,4 +5,22 @@ Circular linked list: A (corrupt) linked list in which a node's next pointer poi
 as to make a loop in the linked list.  */
 
 var circularList = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) {
+            break;
+        }
+    }
+    if (fast === null || fast.next === null) {
+        return null
+    }
+    slow = head;
+    while (slow != fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow
 }
