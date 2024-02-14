0ccdf9d4b3bb5ebcd49bdaf19fe5e75f7b077375
Francis Yang <fy50167@gmail.com>
2024-02-13 20:58:12 -0800

    palindromic list solution

--- linked-lists/palindromicList.js
+++ linked-lists/palindromicList.js
@@ -1,1 +1,28 @@
 /* Implement a function to check if a linked list is a palindrome.  */
+
+var isPalindrome = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let prev = null;
+    let current = slow;
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    while (prev) {
+        if (prev.val !== head.val) {
+            return false
+        }
+        prev = prev.next;
+        head = head.next;
+    }
+    return true
+};
