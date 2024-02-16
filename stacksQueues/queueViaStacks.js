5776244894caa294186fda16e1f625989300d2b5
Francis Yang <fy50167@gmail.com>
2024-02-15 19:20:30 -0800

    queue via stacks solution

--- stacksQueues/queueViaStacks.js
+++ stacksQueues/queueViaStacks.js
@@ -1,1 +1,48 @@
 /* Implement a MyQueue class which implements a queue using two stacks.  */
+
+var MyQueue = function() {
+    this.stackNewest = [];
+    this.stackOldest = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stackNewest.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    reversal(this.stackNewest, this.stackOldest);
+    return this.stackOldest.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    reversal(this.stackNewest, this.stackOldest);
+    return this.stackOldest[this.stackOldest.length - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    if (this.stackNewest == false && this.stackOldest == false) {
+        return true
+    }
+    return false
+};
+
+function reversal(stack1, stack2) {
+    if (stack2.length === 0) {
+        while(stack1.length !== 0) {
+            stack2.push(stack1.pop());
+        }
+    }
+}
